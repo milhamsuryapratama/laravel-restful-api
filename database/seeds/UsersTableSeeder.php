283b0528730bd45060180138c99ac6e6f22a8e46
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-11-07 00:31:56 +0700

    user table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        $password = Hash::make('toptal');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password
+        ]);
+
+        for ($i=0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
