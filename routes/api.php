98f86945c5726cd34497c0efe1c6b86c82186b23
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-11-07 13:26:22 +0700

    membuat register, login, logout route

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,7 @@ Route::get('articles/{article}', 'ArticleController@show');
 Route::post('articles', 'ArticleController@store');
 Route::put('articles/{article}', 'ArticleController@update');
 Route::delete('articles/{article}', 'ArticleController@delete');
+
+Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
